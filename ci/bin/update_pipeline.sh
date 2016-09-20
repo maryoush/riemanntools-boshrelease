0ce777be47b4a0fa9b6460d4c8c8dd70ceed6de2
Maxime Desrosiers <maxime.desrosiers@gmail.com>
2016-09-20 17:17:41 -0400

    refactoring concourse vault secret path

--- ci/bin/update_pipeline.sh
+++ ci/bin/update_pipeline.sh
@@ -15,7 +15,7 @@ fi
 
 CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
 
-pull_credentials riemanntools concourse credentials.yml.erb ${CREDENTIALS}
+vault read -field=value -tls-skip-verify secret/concourse/riemanntools-boshrelease > ${CREDENTIALS}
 
 fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
 if [ $? -ne 0 ]; then
